c00af9dd569b61332c3ef282f37da0178c6a0dbf
bekachachua <beka.chachua@space.ge>
2023-12-23 19:48:33 +0400

    # Transition update

--- app/src/main/java/com/bekka/animations/transition_animation/TransitionAnimationActivity.kt
+++ app/src/main/java/com/bekka/animations/transition_animation/TransitionAnimationActivity.kt
@@ -6,20 +6,36 @@ import androidx.activity.compose.setContent
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDp
+import androidx.compose.animation.core.animateSize
+import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.core.updateTransition
 import androidx.compose.animation.expandIn
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.shrinkOut
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -30,8 +46,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.bekka.animations.R
 import com.bekka.animations.ui.theme.AnimationsTheme
 
 class TransitionAnimationActivity : ComponentActivity() {
@@ -40,7 +59,6 @@ class TransitionAnimationActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AnimationsTheme {
-                // This is the color scheme for your theme. Replace with your actual colors or theme setup.
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
@@ -54,8 +72,6 @@ class TransitionAnimationActivity : ComponentActivity() {
 
 @Composable
 fun AnimatedVisibilityScreen() {
-    var visible by remember { mutableStateOf(true) }
-
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -63,25 +79,73 @@ fun AnimatedVisibilityScreen() {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        AnimatedVisibility(
-            visible = visible,
-            enter = fadeIn(animationSpec = tween(1000)) + expandIn(animationSpec = tween(1000)),
-            exit = fadeOut(animationSpec = tween(1000)) + shrinkOut(animationSpec = tween(1000))
-        ) {
-            Text(
-                text = "Hello my fellow friends from curiosity",
-                style = MaterialTheme.typography.headlineMedium
+        AnimatedImageWithTransition()
+        Spacer(modifier = Modifier.height(20.dp))
+        AnimatedButtonWithTransition()
+    }
+}
+
+private enum class ImageState {
+    Small, Large
+}
+
+@Composable
+fun AnimatedImageWithTransition() {
+
+    var imageState by remember {
+        mutableStateOf(ImageState.Small)
+    }
+
+    val transition = updateTransition(targetState = imageState, label = "BoxState Transition")
+
+    val borderColor by transition.animateColor(label = "BoxState Color Transition") {
+        when (it) {
+            ImageState.Small -> Color.Red
+            ImageState.Large -> Color.Yellow
+        }
+    }
+
+    val size by transition.animateDp(
+        transitionSpec = {
+            spring(
+                dampingRatio = Spring.DampingRatioHighBouncy,
+                stiffness = Spring.StiffnessLow
             )
+        },
+        label = "BoxState Size Transition") {
+        when (it) {
+            ImageState.Small -> 90.dp
+            ImageState.Large -> 130.dp
         }
+    }
 
-        Spacer(modifier = Modifier.height(20.dp))
+    Column(
+        modifier = Modifier.fillMaxWidth().wrapContentHeight(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
 
-        Button(onClick = { visible = !visible }) {
-            Text("Toggle Visibility")
+        Image(
+            modifier = Modifier
+                .size(size = size)
+                .clip(shape = CircleShape)
+                .border(color = borderColor, shape = CircleShape, width = 3.dp),
+            painter = painterResource(id = R.drawable.dragon),
+            contentDescription = "AnimateImage"
+        )
+
+        Button(
+            modifier = Modifier.padding(top = 8.dp),
+            onClick = {
+                imageState =
+                    if (imageState == ImageState.Small)
+                        ImageState.Large
+                    else
+                        ImageState.Small
+            }
+        ) {
+            Text(text = "Toggle State")
         }
-
-        Spacer(modifier = Modifier.height(20.dp))
-        AnimatedButton()
     }
 }
 
@@ -90,7 +154,7 @@ enum class ButtonState {
 }
 
 @Composable
-fun AnimatedButton() {
+fun AnimatedButtonWithTransition() {
     var buttonState by remember { mutableStateOf(ButtonState.Idle) }
 
     val onClick = {
